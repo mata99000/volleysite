bafcc6396e902c39edec11f9d83826b495cc6cbf
John Doe <johndoe@example.com>
2024-07-18 20:33:44 +0200

    Login Page fix

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -51,19 +51,19 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                         </div>
                     ) : (
                         <>
-                            <Link
+                            <a
                                 href={route('login')}
                                 className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                             >
                                 Login
-                            </Link>
+                            </a>
 
-                            <Link
+                            <a
                                 href={route('register')}
                                 className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                             >
                                 Register
-                            </Link>
+                            </a>
                         </>
                     )}
                 </div>
