18f6fa8405d8d470f5bb0269a9d92f025cd44bc3
John Doe <johndoe@example.com>
2024-07-21 00:29:36 +0200

    Tournaments api , name unique v1.0

---
+++ app/Http/Resources/TournamentResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TournamentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'tournament_name' => $this->name,
+            'tournament_date' => $this->start_date,
+            'tournament_location' => $this->location,
+        ];
+    }
+}
