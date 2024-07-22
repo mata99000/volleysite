dc31770ecd17ec8ae18800f8120928b8e0799c89
Milan Mihajlovic <milance_kucevo@hotmail.com>
2024-07-22 08:13:32 +0200

    Final style of table

--- resources/js/Pages/Tournaments/Partials/TournamentList.jsx
+++ resources/js/Pages/Tournaments/Partials/TournamentList.jsx
@@ -24,11 +24,12 @@ export default function TournamentList() {
               <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                 <div className="overflow-hidden">
                 {tournaments.length > 0 ? (
-                <table className="min-w-full text-left text-sm font-light text-surface dark:text-white cursor-pointer">
+                <table className="min-w-full text-center text-sm font-light text-surface dark:text-white cursor-pointer">
                     <tbody>
                         {tournaments.map((tournament) => (
                             <tr key={tournament.tournament_id} className="transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-white/10 dark:hover:bg-neutral-600">
-                                <td className="whitespace-nowrap px-6 py-2"><img className="inline mx-4 h-12 border rounded" src={`tournament-api/${tournament.tournament_pic}`}></img>{tournament.tournament_name}</td>
+                                {/* <td className="whitespace-nowrap py-2 text-center"><img className="inline h-12 border rounded" src={`tournament-api/${tournament.tournament_pic}`}></img></td> */}
+                                <td className="whitespace-nowrap px-6 py-2">{tournament.tournament_name}</td>
                                 <td className="whitespace-nowrap px-6 py-2">{tournament.tournament_date}</td>
                                 <td className="whitespace-nowrap px-6 py-2">{tournament.tournament_location}</td>
                             </tr>
