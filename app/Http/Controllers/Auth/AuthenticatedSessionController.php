e0e7c813df0fa6a4310b1787b3af5214c8cc588a
Mateja <contact@matejamihajlovic.com>
2024-10-03 14:25:21 +0200

    Redirect to /login after Logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -47,6 +47,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
 }
