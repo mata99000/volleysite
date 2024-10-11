8c80b48a0e98648457734cc6fe9c1e4a476799fa
Milan Mihajlovic <milance_kucevo@hotmail.com>
2024-10-11 22:52:03 +0200

    Kod za upload Cover image. !!Ne radi!!

--- resources/js/Pages/Profile/Profile.jsx
+++ resources/js/Pages/Profile/Profile.jsx
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { usePage } from '@inertiajs/react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import ProfileTabs from '@/Pages/Profile/ProfilePartials/ProfileTabs';
 import PrimaryButton from '@/Components/PrimaryButton';
+import axios from 'axios';
 
 export default function Profile({ auth }) {
     const { users_profile } = usePage().props;
+    const { coverPreview, setCoverPreview } = useState(null);
+    const { selectedCover, setSelectedCover } = useState(false);
+    const { userCoverImage, setUserCoverImage } = useState(users_profile.cover_image);
+    const { errors, setErrors} = useState({});
 
-    // Funkcija za formatiranje datuma rođenja i računanje starosti
-    const formatDate = (dateString) => {
-        const date = new Date(dateString);
-        const day = date.getDate();
-        const month = date.toLocaleString('default', { month: 'long' });
-        const year = date.getFullYear();
+    const onCoverChange = (e) => {
+        const file = e.target.files[0];
+        setSelectedCover(true);
 
-        // Izračunavanje starosti korisnika
-        const today = new Date();
-        let age = today.getFullYear() - year;
-        const m = today.getMonth() - date.getMonth();
-        if (m < 0 || (m === 0 && today.getDate() < day)) {
-            age--;
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setCoverPreview(reader.result);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            setCoverPreview(null);
+            setSelectedCover(false);
         }
-
-        return `${day} ${month} ${year} (${age} years old)`;
     };
 
-    
+    const handleSubmitCover = async (e) => {
+        e.preventDefault();
+        const file = e.target.cover_image.files[0];
+
+        const formData = new FormData();
+        formData.append('cover_image', file);
+
+        try {
+            const response = await axios.post(route('profile.set_cover_image'), formData, {
+                headers: {
+                    'Content-Type' : 'multipart/form-data',
+                }
+            });
+        if (response.status === 200) {
+            setCoverPreview(response.data.cover_image_url);
+            setUserCoverImage(response.data.cover_image_name);
+            setSelectedCover(false);                
+            setErrors({}); // Očistite greške
+            }
+        } catch (error) {
+            if (error.response && error.response.status === 422) {
+                setErrors(error.response.data.errors);
+            } else {
+                console.error('Error uploading image:', error);
+                alert('Greska prilikom uploada');
+            }
+
+        }
+    };
 
     return (
         <AuthenticatedLayout
@@ -36,9 +67,11 @@ export default function Profile({ auth }) {
             <Head title={`${users_profile.name} ${users_profile.lastname} - Profile`} />
 
             <div className="mx-auto container">
+                <form onSubmit={handleSubmitCover}>
                 <div className="relative group">
-                    <img src="https://img.freepik.com/free-photo/high-angle-composition-with-volleyballs_23-2149023837.jpg?t=st=1723222142~exp=1723225742~hmac=923b49631a618fe56394d502f79b21ad672b5323fef3e1160c068bbc6f98a3f9&w=1380"
+                    <img id="cover-image" src={coverPreview || (userCoverImage ? `/storage/covers/${userCoverImage}` : `/storage/covers/volley-background.png`)}
                      className="w-full h-[350px] object-cover"></img>
+                     <div className="flex">
                      <button className="absolute top-2 right-2 bg-gray-50 hover:bg-gray-100 text-gray-800 py-1 px-2 text-xs flex items-center opacity-0 group-hover:opacity-100">
                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4 mr-2">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
@@ -46,7 +79,7 @@ export default function Profile({ auth }) {
                     </svg>
 
                         Update Cover Image
-                        <input type="file" accept="image/*" className="absolute left-0 top-0 bottom-0 right-0 opacity-0 cursor-pointer"></input>
+                        <input onChange={onCoverChange} type="file" accept="image/*" id="cover_image" name="cover_image" className="absolute left-0 top-0 bottom-0 right-0 opacity-0 cursor-pointer"></input>
                      </button>
                     <div className="flex">
                             <img src={`/storage/avatars/${users_profile.image_name}`}
@@ -56,7 +89,15 @@ export default function Profile({ auth }) {
 
                         </div>
                     </div>
-                </div>   
+                </div> 
+                { selectedCover && (
+                        <button type="submit" className="absolute top-2 right-2 bg-gray-50 hover:bg-gray-100 text-gray-800 py-1 px-2 text-xs flex items-center opacity-0 group-hover:opacity-100">
+                           Save
+                        </button>
+                     )} 
+                     </div>
+                </form>
+
                     <div className="w-full px-2 sm:px-0 border-t">
                             <ProfileTabs />
                     </div>
